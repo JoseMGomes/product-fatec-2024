0f5b93eed49b7ab81769cdb69321b6a827975ed4
José Lucas <gomessze03@gmail.com>
2024-09-03 08:58:42 -0300

    the product component was changed

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,12 +1,12 @@
-import { Product } from './../product';
 import { Component } from '@angular/core';
+import { Product } from '../Product';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css',
+  selector: 'app-products-table',
+  templateUrl: './products-table.component.html',
+  styleUrl: './products-table.component.css'
 })
-export class ProductComponent {
+export class ProductsTableComponent {
   products: Product[] = [
     { id: 1, name: 'Computador I7', price: 6000.0, category: 'Eletronicos' },
     { id: 1, name: 'Computador I5', price: 5000.0, category: 'Eletronicos' },

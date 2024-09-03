a076b6918706e0723c3fa0d9c18fe2fe26cad1d4
José Lucas <gomessze03@gmail.com>
2024-09-03 11:14:51 -0300

    json.server was configured and used to simulate the API

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../Product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products-table',
   templateUrl: './products-table.component.html',
-  styleUrl: './products-table.component.css'
+  styleUrl: './products-table.component.css',
 })
-export class ProductsTableComponent {
-  products: Product[] = [
-    { id: 1, name: 'Computador I7', price: 6000.0, category: 'Eletronicos' },
-    { id: 1, name: 'Computador I5', price: 5000.0, category: 'Eletronicos' },
-    { id: 1, name: 'Mouse', price: 100, category: 'Eletronicos' },
-  ];
+export class ProductsTableComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private service: ProductService) {}
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.service.getProducts().subscribe({
+      next: (data) => (this.products = data),
+    });
+  }
 }
